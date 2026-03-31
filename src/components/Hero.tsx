b8f7f02f3d5ed2d93977852c69ff322ebe42c64b
N!kola Igic <igic.nikola01@gmail.com>
2026-03-31 14:32:28 +0200

    hero almost done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,34 @@
+"use client";
+
 import styles from "./Hero.module.css";
+import anim from "./HeroAnimation.module.css";
 import Image from "next/image";
 import card from "../../public/hero/card.png";
 import envelop from "../../public/hero/envelop.png";
-import flowers from "../../public/hero/group_flowers.png";
 import flowersOnPaper from "../../public/hero/flowers_on_paper.png";
 import frontEnvelop from "../../public/hero/front_envelop.png";
 import photo from "../../public/hero/their_photo.png";
 
 export default function Header() {
     return (
-        <main className={styles.hero}>
+        <section className={styles.hero}>
+            <span className={`${styles.scriptName} ${styles.katka} ${anim.scriptNameBase} ${anim.scriptNameAnimate}`}>
+                Katka
+            </span>
+            <span className={`${styles.scriptName} ${styles.jaro} ${anim.scriptNameBase} ${anim.scriptNameAnimate} ${anim.jaroDelay}`}>
+                Jaro
+            </span>
+
             {/* Envelop */}
 
-            <div className={styles.envelop}>
+            <div className={`${styles.envelop} ${anim.envelopStart} ${anim.envelopLanding}`}>
                 <Image src={envelop} alt="envelop" className={styles.enve} />
                 <Image
                     src={frontEnvelop}
                     alt="front part of envelop"
                     className={styles.enveFront}
                 />
-                <Image src={card} alt="card" className={styles.card} />
+                <Image src={card} alt="svadobna pozvanka" className={styles.card} />
                 <Image
                     src={photo}
                     alt="photo of Katka and Jaro"
@@ -31,9 +40,6 @@ export default function Header() {
                     className={styles.flowersOnPaper}
                 />
             </div>
-
-            {/* Flowers */}
-            <Image src={flowers} alt="flowers" className={styles.flowers} />
-        </main>
+        </section>
     );
 }
